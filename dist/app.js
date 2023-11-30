dcf7e40cba5959cf3cfb4a17db3d4358a457a0e1
LuisBuis22 <luis.buisan@gmail.com>
2023-11-30 13:05:38 +0100

    Primer Commit API todo

---
+++ dist/app.js
@@ -0,0 +1,43 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.json());
+let todos = [];
+app.get("/todos", (_req, res) => {
+    res.json(todos);
+});
+app.post("/todos", (req, res) => {
+    const todo = {
+        id: todos.length + 1,
+        title: req.body.title,
+        completed: req.body.completed || false,
+    };
+    todos.push(todo);
+    res.status(201).json(todo);
+});
+app.put("/todos/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const todo = todos.find((t) => t.id === id);
+    if (!todo) {
+        return res.status(404).json({ error: "Tarea no encontrada" });
+    }
+    todo.title = req.body.title || todo.title;
+    todo.completed = req.body.completed || todo.completed;
+    res.json(todo);
+});
+app.delete("/todos/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = todos.findIndex((t) => t.id === id);
+    if (index === -1) {
+        return res.status(404).json({ error: "Tarea no encontrada" });
+    }
+    todos.splice(index, 1);
+    res.status(204).send();
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,54 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+import { Request, Response } from "express";
+
+const app = express();
+
+type Todos = {
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+app.use(bodyParser.json());
+let todos: Todos[] = [];
+
+app.get("/todos", (_req: Request, res: Response) => {
+  res.json(todos);
+});
+
+app.post("/todos", (req: Request, res: Response) => {
+  const todo = {
+    id: todos.length + 1,
+    title: req.body.title,
+    completed: req.body.completed || false,
+  };
+  todos.push(todo);
+  res.status(201).json(todo);
+});
+
+app.put("/todos/:id", (req: Request, res: Response) => {
+  const id = parseInt(req.params.id);
+  const todo = todos.find((t) => t.id === id);
+  if (!todo) {
+    return res.status(404).json({ error: "Tarea no encontrada" });
+  }
+  todo.title = req.body.title || todo.title;
+  todo.completed = req.body.completed || todo.completed;
+  res.json(todo);
+});
+
+app.delete("/todos/:id", (req: Request, res: Response) => {
+  const id = parseInt(req.params.id);
+  const index = todos.findIndex((t) => t.id === id);
+  if (index === -1) {
+    return res.status(404).json({ error: "Tarea no encontrada" });
+  }
+  todos.splice(index, 1);
+  res.status(204).send();
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
